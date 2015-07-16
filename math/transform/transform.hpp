abb776f8a25ca823fd10805bb5be47b500aeb4d1
Phil Cooper-King <phil@cooperking.net>
2015-07-16 08:51:52 +0100

    Started work on transform test, but time to got kitesurfing.

--- math/transform/transform.hpp
+++ math/transform/transform.hpp
@@ -12,9 +12,41 @@ namespace caffeine {
 namespace math {
 
 
-inline matrix44   get_world_matrix(const transform &transform);
-inline void       set_with_world_matrix(transform &transform, const matrix44 &matrix);
-inline transform  inherit_parent_transform(const transform &parent, const transform &child);
+// ** Interface ** //
+
+inline transform  transform_init(const vector3 position, const vector3 scale, const quaternion &scale);
+inline matrix44   transform_get_world_matrix(const transform &transform);
+inline void       transform_set_with_world_matrix(transform &transform, const matrix44 &matrix);
+inline transform  transform_inherited(const transform &parent, const transform &child);
+
+
+// ** Impl ** //
+
+
+transform
+transform_init(const vector3 position, const vector3 scale, const quaternion &rotation)
+{
+  transform return_transform;
+  return_transform.position = position;
+  return_transform.scale    = scale;
+  return_transform.rotation = rotation;
+
+  return return_transform;
+}
+
+
+transform
+transform_inherited(const transform &parent, const transform &child)
+{
+  transform inherited;
+
+  inherited.scale    = caffmath::vector3_multiply(parent.scale, child.scale);
+  inherited.position = caffmath::vector3_subtract(parent.position, child.position);
+
+  // TODO: Rotation.
+
+  return inherited;
+}
 
 
 } // namespace
---
+++ test/transform_unit_test.cpp
@@ -0,0 +1,23 @@
+#include <unit_test/unit_test.hpp>
+#include <math/transform/transform.hpp>
+#include <test_asserts.hpp>
+
+
+namespace
+{
+  const caffmath::transform test_data_01;
+}
+
+
+TEST(transform_inherited)
+{
+
+}
+
+
+int main()
+{
+
+
+  return 0;
+}
