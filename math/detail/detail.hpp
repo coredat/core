5f58bbe77bcb9d19d30dcdf3c2dfec6340cb718e
Phil Cooper-King <phil@cooperking.net>
2016-01-31 20:48:16 +0000

    small change.

--- math/detail/detail.hpp
+++ math/detail/detail.hpp
@@ -4,6 +4,7 @@
 
 #ifdef MATH_ENABLE_SIMD
 #define MATH_NO_SIMD
+#define MATH_SSE2
 #endif
 
 
@@ -14,12 +15,19 @@
 #endif
 
 
+#define MATH_CONST __attribute__((const))
+
+#define MATH_LIKELY(x)       __builtin_expect((x),1)
+#define MATH_UNLIKELY(x)     __builtin_expect((x),0)
+
+
 #define MATH_VEC2_INLINE inline
 #define MATH_VEC3_INLINE inline
 #define MATH_VEC4_INLINE inline
 #define MATH_MAT3_INLINE inline
 #define MATH_MAT4_INLINE inline
 #define MATH_QUAT_INLINE inline
+#define MATH_GENR_INLINE inline
 
 
 #endif // include guard
--- math/general/general.hpp
+++ math/general/general.hpp
@@ -10,6 +10,7 @@
 
 #include <cmath>
 #include <algorithm>
+#include "../detail/detail.hpp"
 
 
 namespace math {
@@ -20,46 +21,49 @@ namespace math {
 
 // ** Constants ** //
 
-inline float              pi()          { return 3.14159265359f;  }
-inline float              two_pi()      { return 2.f * pi();      }
-inline float              half_pi()     { return pi() * 0.5f;     }
-inline float              tau()         { return two_pi();        }
-inline float              half_tau()    { return pi();            }
-inline float              quart_tau()   { return half_pi();       }
-inline float              g_ratio()     { return 1.61803398875f;  }
-inline float              root_two()    { return 1.41421356237f;  }
-inline float              root_three()  { return 1.73205080757f;  }
+MATH_GENR_INLINE float              pi()          { return 3.14159265359f;  }
+MATH_GENR_INLINE float              two_pi()      { return 2.f * pi();      }
+MATH_GENR_INLINE float              half_pi()     { return pi() * 0.5f;     }
+MATH_GENR_INLINE float              tau()         { return two_pi();        }
+MATH_GENR_INLINE float              half_tau()    { return pi();            }
+MATH_GENR_INLINE float              quart_tau()   { return half_pi();       }
+MATH_GENR_INLINE float              g_ratio()     { return 1.61803398875f;  }
+MATH_GENR_INLINE float              root_two()    { return 1.41421356237f;  }
+MATH_GENR_INLINE float              root_three()  { return 1.73205080757f;  }
 
 
 // ** Trig ** //
 
-inline float              tan(const float x);
-inline float              a_tan2(const float x, const float y);
-inline float              cos(const float radians);
-inline float              a_cos(const float radians);
-inline float              sin(const float radians);
-inline float              a_sin(const float radians);
+MATH_GENR_INLINE float              tan(const float x);
+MATH_GENR_INLINE float              a_tan2(const float x, const float y);
+MATH_GENR_INLINE float              cos(const float radians);
+MATH_GENR_INLINE float              a_cos(const float radians);
+MATH_GENR_INLINE float              sin(const float radians);
+MATH_GENR_INLINE float              a_sin(const float radians);
 
 
 // ** Degs/Rads ** //
 
-inline float              radians_to_degrees(const float radians); // TODO:
-inline float              degrees_to_radians(const float degrees); // TODO:
+MATH_GENR_INLINE float              radians_to_degrees(const float radians); // TODO:
+MATH_GENR_INLINE float              degrees_to_radians(const float degrees); // TODO:
 
 
 // ** Other general ** //
 
-inline float              sqrt(const float x);
-inline float              abs(const float x);
-inline float              max(const float a, const float b); // TODO:
-inline float              min(const float a, const float b); // TODO:
-inline float              clamp(const float x, const float between_a, const float between_b);
-inline bool               is_between(const float to_check, const float a, const float b); // TODO:
-inline bool               is_near(const float actual, const float target, const float error_margin); // TODO: 
-inline bool               is_pow_two(const uint32_t i); // TODO: 
-inline bool               fmod(const float x, const float max); // TODO;
-inline float              sign(const float x); // Returns 1 or -1
+MATH_GENR_INLINE float              sqrt(const float x);
+MATH_GENR_INLINE float              abs(const float x);
+MATH_GENR_INLINE float              max(const float a, const float b); // TODO:
+MATH_GENR_INLINE float              min(const float a, const float b); // TODO:
+MATH_GENR_INLINE float              clamp(const float x, const float between_a, const float between_b);
+MATH_GENR_INLINE bool               is_between(const float to_check, const float a, const float b); // TODO:
+MATH_GENR_INLINE bool               is_near(const float actual, const float target, const float error_margin); // TODO:
+MATH_GENR_INLINE bool               is_pow_two(const uint32_t i); // TODO:
+MATH_GENR_INLINE bool               fmod(const float x, const float max); // TODO;
+MATH_GENR_INLINE float              sign(const float x); // Returns 1 or -1
 
+
+// ** Search ** //
+MATH_GENR_INLINE bool               index_linear_search(const uint32_t to_find, const uint32_t search[], const size_t size_of_search, size_t *out_result);
 
 
 // ** IMPL ** //
@@ -159,6 +163,23 @@ sign(const float x)
 {
   return x >= 0.f ? 1.f : -1.f;
 }
+
+
+bool
+index_linear_search(const uint32_t to_find, const uint32_t search[], const size_t size_of_search, size_t *out_result)
+{
+  for(std::size_t i = 0; i < size_of_search; ++i)
+  {
+    if(search[i] == to_find)
+    {
+      (*out_result) = i;
+      return true;
+    }
+  }
+
+  return false;
+}
+
 
 } // namespace
 
