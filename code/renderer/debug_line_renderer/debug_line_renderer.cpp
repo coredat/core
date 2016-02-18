6f9a6d84ffd878c9bcc8a92b68285282e1547eed
Phil Cooper-King <phil@cooperking.net>
2016-02-18 08:27:26 +0000

    Cleanup of debug line renderer, still need to check some stuff however.

--- code/renderer/debug_line_renderer/debug_line_renderer.cpp
+++ code/renderer/debug_line_renderer/debug_line_renderer.cpp
@@ -20,12 +20,13 @@ namespace
   GLint uni_wvp;
   GLint uni_data;
   
-  GLint uni_line[128];
+  const size_t line_uniform_max = 32 * 3; // 64 lines, 3 components per line (start, end, color)
+  GLint uni_line[32 * 3];
   
-  std::vector<float> data;
-  std::size_t data_ptr = 0;
+  const size_t size_of_data_buffer = (1 << 20) * 3; // Some big size * number of components (start, end, color)
+  float data[(1 << 20) * 3];
   
-  Graphics_api::Texture_filtering filtering;
+  size_t data_ptr = 0;
 }
 
 
@@ -58,29 +59,30 @@ initialize()
   
     uniform mat4        uni_wvp_mat;
     uniform sampler2D 	uni_data_lookup;
-  
-    uniform vec3        uni_line[128];
+
+    #define NUM_LINES 32
+    #define COMPONENTS_PER_LINE 3
+
+    uniform vec3        uni_line[NUM_LINES * COMPONENTS_PER_LINE]; // In groups of three, start, end, color.
   
     out vec3            ps_in_color;
 
     void main()
     {
-//      int row = gs_in_vert_id[0] / 1024;
-//      int col = (gs_in_vert_id[0] % 1024) * 4;
-//    
-//      vec4 start_point  = texelFetch(uni_data_lookup, ivec2(col + 0, row), 0);
-//      vec4 end_point    = texelFetch(uni_data_lookup, ivec2(col + 1, row), 0);
-//      vec4 color        = texelFetch(uni_data_lookup, ivec2(col + 2, row), 0);
-//      //vec4 padding      = texelFetch(uni_data_lookup, ivec2(col + 4, row), 0);
-//
-      int id = gs_in_vert_id[0] * 4;
+      int id = gs_in_vert_id[0] * COMPONENTS_PER_LINE;
+      
+      int start = id + 0;
+      int end   = id + 1;
+      int color = id + 2;
+      
+      ps_in_color = uni_line[color];
+      
+      // Generate the primitive //
 
-      ps_in_color = uni_line[id + 2];
-
-      gl_Position = uni_wvp_mat * vec4(uni_line[id], 1);
+      gl_Position = uni_wvp_mat * vec4(uni_line[start], 1);
       EmitVertex();
       
-      gl_Position = uni_wvp_mat * vec4(uni_line[id + 1], 1);
+      gl_Position = uni_wvp_mat * vec4(uni_line[end], 1);
       EmitVertex();
       
       EndPrimitive();
@@ -107,37 +109,15 @@ initialize()
   Ogl::Shader_uniforms uniforms;
   Ogl::shader_uniforms_retrive(&uniforms, &debug_line_shader);
   Ogl::error_check("Getting uniforms from debug shader", &std::cout);
-
-  // Size the data container.
-  {
-    const std::size_t width_of_data         = 1024;
-    const std::size_t height_of_data        = width_of_data;
-    const std::size_t number_of_components  = 4;
-    
-    //data.resize((width_of_data * height_of_data) * number_of_components, 0);
-    data.resize((1 << 20) * 12);
-  
-   // const auto format = Ogl::pixel_format_get_gl_internal_format(Graphics_api::Pixel_format::rgba32f);
-   // Ogl::texture_create_2d(&data_texture, width_of_data, height_of_data, format, (void*)data.data(), &std::cout);
-  }
   
   Ogl::shader_uniforms_get_uniform_index(&uni_wvp, &uniforms, "uni_wvp_mat");
   Ogl::shader_uniforms_get_uniform_index(&uni_data, &uniforms, "uni_data_lookup");
   
-  for(size_t i = 0; i < 128; ++i)
+  for(size_t i = 0; i < line_uniform_max; ++i)
   {
     const std::string uni_name = "uni_line[" + std::to_string(i) + "]";
     uni_line[i] = glGetUniformLocation(debug_line_shader.program_id, uni_name.c_str());
-
-    //Ogl::shader_uniforms_get_uniform_index(&uni_line[i], &uniforms, uni_name);
   }
-  
-  filtering = Graphics_api::Texture_filtering
-  {
-    Graphics_api::Wrap_mode::clamp,
-    Graphics_api::Wrap_mode::clamp,
-    Graphics_api::Filtering_mode::point
-  };
   
   Ogl::error_check("Debug Renderer Setup.", &std::cout);
 }
@@ -149,24 +129,23 @@ add_lines(const Line_node nodes[], const std::size_t number_of_lines)
   // Shit - can do two memcpy's instead
   for(int32_t i = 0; i < number_of_lines; ++i)
   {
-    const std::size_t start_index = (data_ptr * (16)) % data.size();
+    if(data_ptr >= size_of_data_buffer)
+    {
+      assert(false); // trying to render more lines than we have capacity for.
+      return;
+    }
     
-    data.at(start_index + 0) = nodes[i].position_from[0];
-    data.at(start_index + 1) = nodes[i].position_from[1];
-    data.at(start_index + 2) = nodes[i].position_from[2];
-    data.at(start_index + 3) = 1;
+    data[data_ptr++] = nodes[i].position_from[0];
+    data[data_ptr++] = nodes[i].position_from[1];
+    data[data_ptr++] = nodes[i].position_from[2];
     
-    data.at(start_index + 4) = nodes[i].position_to[0];
-    data.at(start_index + 5) = nodes[i].position_to[1];
-    data.at(start_index + 6) = nodes[i].position_to[2];
-    data.at(start_index + 7) = 1;
+    data[data_ptr++] = nodes[i].position_to[0];
+    data[data_ptr++] = nodes[i].position_to[1];
+    data[data_ptr++] = nodes[i].position_to[2];
     
-    data.at(start_index + 8)  = nodes[i].color[0];
-    data.at(start_index + 9)  = nodes[i].color[1];
-    data.at(start_index + 10) = nodes[i].color[2];
-    data.at(start_index + 11) = 1;
-    
-    data_ptr++;
+    data[data_ptr++] = nodes[i].color[0];
+    data[data_ptr++] = nodes[i].color[1];
+    data[data_ptr++] = nodes[i].color[2];
   }
 }
 
@@ -174,15 +153,13 @@ add_lines(const Line_node nodes[], const std::size_t number_of_lines)
 void
 render(const float wvp_mat[16])
 {
-  // Update texture
-  // Ogl::texture_update_texture_2d(&data_texture, 0, 0, data_texture.width, data_texture.height, (void*)data.data(), &std::cout);
-  // Ogl::error_check("Updating texture", &std::cout);
-
-  const size_t number_to_batch = data_ptr % 128;
+  const size_t number_to_batch = (data_ptr / 32) + 1;
   
   // Render
   Ogl::reset_state();
   
+  static size_t data_get = 0;
+  data_get = 0;
   
   for(size_t b = 0; b < number_to_batch; ++b)
   {
@@ -192,26 +169,25 @@ render(const float wvp_mat[16])
     glUniformMatrix4fv(uni_wvp, 1, GL_FALSE, wvp_mat);
     Ogl::error_check("set wvp.", &std::cout);
     
-    static int uniform_index = 0;
-    uniform_index = 0;
     
-    for(size_t u = 0; u < 128; ++u)
+    for(size_t l = 0; l < 32; ++l)
     {
-      size_t i = (u * 4) + (b * (128 * 4));
-      glUniform3f(uni_line[u], data.at(i + 0), data.at(i + 1), data.at(i + 2));
-      Ogl::error_check("Uploading uniforms", &std::cout);
+      const size_t uni = l * 3;
+      
+      glUniform3f(uni_line[uni + 0], data[data_get+ 0], data[data_get+ 1], data[data_get+2]);
+      glUniform3f(uni_line[uni + 1], data[data_get+ 3], data[data_get+ 4], data[data_get+5]);
+      glUniform3f(uni_line[uni + 2], data[data_get+ 6], data[data_get+ 7], data[data_get+8]);
+      
+      data_get += 9;
+      
+      Ogl::error_check("Uploading debug line uniforms", &std::cout);
     }
     
-//    glActiveTexture(GL_TEXTURE0 + uni_data);
-//    glBindTexture(GL_TEXTURE_2D, data_texture.texture_id);
-
-//    Ogl::filtering_apply(filtering);
-    
-    glDrawArrays(GL_POINTS, 0, static_cast<GLsizei>(128));
+    glDrawArrays(GL_POINTS, 0, static_cast<GLsizei>(line_uniform_max));
   }
   
   data_ptr = 0;
-  memset(data.data(), 0, sizeof(float) * data.size());
+  memset(data, 0, sizeof(float) * size_of_data_buffer);
 
   Ogl::error_check("Debug line renderer.", &std::cout);
 }
