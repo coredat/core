e70295ec65df062fae2ceab9a0763af76cc66eb1
Phil Cooper-King <phil@cooperking.net>
2016-03-23 13:44:25 +0000

    Added data members to world.

--- code/core/world/world.hpp
+++ code/core/world/world.hpp
@@ -6,6 +6,8 @@
 #include "entity.hpp"
 #include "entity_ref.hpp"
 #include <stdint.h>
+#include <data/core_data/core_data.hpp>
+#include <data/world_data/world_data.hpp>
 
 
 namespace Core {
@@ -15,10 +17,15 @@ class World
 {
 public:
 
-  explicit          World(const World_setup &setup);
+  explicit              World(const World_setup &setup);
 
-  Entity            create_entity();
-  Entity_ref        find_entity(const char *name);
+  Entity                create_entity();
+  Entity_ref            find_entity(const char *name);
+  
+private:
+
+  Core_data::Core       m_core_data;
+  World_data::World     m_world_data;
 
 };
 
