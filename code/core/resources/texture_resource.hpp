63c13fc462ea94d9a69292f6569fa1d45f89ad4a
Phil Cooper-King <phil@cooperking.net>
2016-03-30 09:59:19 +0100

    started to add resources in.

---
+++ code/core/resources/texture_resource.hpp
@@ -0,0 +1,19 @@
+#ifndef TEXTURE_RESOURCE_INCLUDED_AF43B4CA_12E2_4179_B710_EF91EA9BFF1E
+#define TEXTURE_RESOURCE_INCLUDED_AF43B4CA_12E2_4179_B710_EF91EA9BFF1E
+
+
+#include <stdint.h>
+
+
+namespace Core {
+namespace Resource {
+
+
+uint32_t        texture_load(const char *filename);
+
+
+} // ns
+} // ns
+
+
+#endif // inc guard
