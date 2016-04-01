4466e9397f3d651e0c6dbb7221a6b0f1754f9ba3
Phil Cooper-King <phil@cooperking.net>
2016-04-01 20:08:24 +0100

    Added better logging output.

--- code/external/utilities/utilities/logging.cpp
+++ code/external/utilities/utilities/logging.cpp
@@ -2,28 +2,45 @@
 #include <iostream>
 
 
+namespace
+{
+  uint32_t logging_outputs = util::log_out::console;
+}
+
+
 namespace util {
+
+
+void
+set_output(const uint32_t output)
+{
+  logging_outputs = output;
+}
 
 
 void
 log(const char *prefix,
     const char *msg,
     const char *file,
+    const char *func,
     const uint32_t line)
 {
-  if(prefix)        { std::cout << prefix << " "; }
-  if(file && line)  { std::cout << file << " : " << line << " "; }
-  
-  if((prefix || (file && line)) && msg)
+  if(logging_outputs & util::log_out::console)
   {
-    std::cout << " - ";
-  }
-  
-  if(msg) { std::cout << msg << " "; }
-  
-  if(prefix || (file && line) || msg)
-  {
-    std::cout << std::endl;
+    if(prefix)        { std::cout << prefix << " "; }
+    if(file && line)  { std::cout << file << " : " << func << " : " << line << " "; }
+    
+    if((prefix || (file || func)) && msg)
+    {
+      std::cout << std::endl;;
+    }
+    
+    if(msg) { std::cout << "> " << msg << " "; }
+    
+    if(prefix || (file && line) || msg)
+    {
+      std::cout << std::endl;
+    }
   }
 }
 
--- code/external/utilities/utilities/logging.hpp
+++ code/external/utilities/utilities/logging.hpp
@@ -5,19 +5,41 @@
 #include <stdint.h>
 
 
-#define LOG_INFO(msg) util::log("[info]", msg, __FUNCTION__, __LINE__);
-#define LOG_WARNING(msg) util::log("[warn]", msg, __FUNCTION__, __LINE__);
-#define LOG_ERROR(msg) util::log("[err]", msg, __FUNCTION__, __LINE__);
-#define LOG_FATAL(msg) util::log("[fatal]", msg, __FUNCTION__, __LINE__);
+#if defined __APPLE__ || __linux__
+  #define LOGGING_FUNC_STR __PRETTY_FUNCTION__
+#else
+  #define LOGGING_FUNC_STR __FUNCTION__
+#endif
+
+#define LOGGING_FILE_NAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define LOG_INFO(msg) util::log("[info]", msg, LOGGING_FILE_NAME, LOGGING_FUNC_STR, __LINE__);
+#define LOG_WARNING(msg) util::log("[warn]", msg, LOGGING_FILE_NAME, LOGGING_FUNC_STR, __LINE__);
+#define LOG_ERROR(msg) util::log("[err]", msg, LOGGING_FILE_NAME, LOGGING_FUNC_STR, __LINE__);
+#define LOG_FATAL(msg) util::log("[fatal]", msg, LOGGING_FILE_NAME, LOGGING_FUNC_STR, __LINE__);
 
 
 namespace util {
+
+
+namespace log_out {
+enum ENUM {
+
+  console = 1 << 0,
+
+};
+}
+
+
+void
+set_output(const uint32_t out);
 
 
 void
 log(const char *prefix,
     const char *msg,
     const char *file,
+    const char *func,
     const uint32_t line);
 
 } // util
