eb9f7772145f48a83004a3c48920d7af130553cd
Phil Cooper-King <phil@cooperking.net>
2016-04-01 07:23:37 +0100

    Squashed 'code/external/math/' changes from 709282f..cc6d5df
    cc6d5df Readded genral.
    af3ea61 Readded files that got pruned out by gitnore.
    b036ab9 Added updated git ignore.
    
    git-subtree-dir: code/external/math
    git-subtree-split: cc6d5dfdf3f7386b435139b875dc505610341058

---
+++ math/geometry/geometry_fwd.hpp
@@ -0,0 +1,15 @@
+#ifndef GEOMETRY_FWD_INCLUDED_90B3513F_2B5F_4C02_8AB5_2B82BFD864C6
+#define GEOMETRY_FWD_INCLUDED_90B3513F_2B5F_4C02_8AB5_2B82BFD864C6
+
+
+namespace math {
+
+
+struct aabb;
+struct ray;
+
+
+} // ns
+
+
+#endif // inc guard
