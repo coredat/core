c04d86e97cde03101c8b55032c66dec7bc6f2fc7
Phil Cooper-King <phil@cooperking.net>
2016-04-04 07:50:57 +0100

    Squashed 'code/external/graphics_api/' changes from da2c1ab..789ce8c
    789ce8c Added ability to query bits in a pixel format.
    
    git-subtree-dir: code/external/graphics_api
    git-subtree-split: 789ce8c9b6ca03d7ddc4b36a1d2152784e236b8e

--- graphics_api/pixel_format.hpp
+++ graphics_api/pixel_format.hpp
@@ -10,11 +10,41 @@ namespace Graphics_api {
 
 enum class Pixel_format : uint8_t
 {
+  rgb8,
   rgba8,
   rgba32f,
+  r32f,
   
   unknown,
 };
+
+
+/*!
+  Returns the number of red bits in a format.
+*/
+uint32_t
+pixel_format_red_bits(const Pixel_format fmt);
+
+
+/*!
+  Returns the number of green bits in a format.
+*/
+uint32_t
+pixel_format_green_bits(const Pixel_format fmt);
+
+
+/*!
+  Returns the number of blue bits in a format.
+*/
+uint32_t
+pixel_format_blue_bits(const Pixel_format fmt);
+
+
+/*!
+  Returns the number of alpha bits in a format.
+*/
+uint32_t
+pixel_format_alpha_bits(const Pixel_format fmt);
 
 
 } // ns
