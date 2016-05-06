9509f8cdd3f82f00d6f7e6afa7bbbab3156c35c6
Phil Cooper-King <phil@cooperking.net>
2016-05-06 20:41:23 +0100

    Small change.

--- code/data/world_data/physics_data.hpp
+++ code/data/world_data/physics_data.hpp
@@ -12,9 +12,12 @@
 namespace World_data {
 
 
+/*!
+  The entities physics simulation.
+*/
 struct Physics_data
 {
-  util::generic_id     *entity_id      = nullptr;
+  util::generic_id    *entity_id      = nullptr;
   math::transform     *transform      = nullptr;
   math::aabb          *aabb_collider  = nullptr;
   
@@ -23,34 +26,58 @@ struct Physics_data
 };
 
 
+/*!
+  Sets up the entity pool
+  \param data The physics data.
+  \param size How big the physics data should be.
+*/
 void
-physics_init(Physics_data *data, const uint32_t size);
+physics_init(Physics_data *data,
+             const uint32_t size);
 
 
+/*!
+  Frees the entity pool.
+  \param data The data to free.
+*/
 void
 physics_free(Physics_data *data);
 
 
+/*!
+  Add an entity to The physics simulation data.
+  \param data The data to free.
+  \param entity_id Is the id of the entity.
+  \param aabb The current aabb of the entity.
+*/
 void
 physics_add(Physics_data *data,
-            const util::generic_id id,
+            const util::generic_id entity_id,
             const math::aabb *aabb,
             const math::transform *trans);
 
 
-void
-physics_remove(Physics_data *data,
-               const util::generic_id id);
-
-
+/*!
+  Update an entity in the simulation.
+  \param data The Physics simulation data.
+  \param entity_id The entity which needs updating.
+  \param trans The entity transform.
+*/
 void
 physics_update(Physics_data *data,
-               const util::generic_id id,
+               const util::generic_id entity_id,
                const math::aabb *aabb,
                const math::transform *trans);
 
-uint32_t
-physics_find_index(Physics_data *data, const util::generic_id id);
+
+/*!
+  Pull an entity out of the physics simulation.
+  \param data The Phyiscs simulation data.
+  \param entity_id The entity to remove.
+*/
+void
+physics_remove(Physics_data *data,
+               const util::generic_id entity_id);
 
 
 } // ns
