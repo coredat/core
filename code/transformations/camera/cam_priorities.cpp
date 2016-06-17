aa4925c846cd1ce5c365d195aa50cd741e53bb2b
Phil Cooper-King <phil@cooperking.net>
2016-06-17 14:09:23 +0100

    Something wrong with camera priorities.

--- code/transformations/camera/cam_priorities.cpp
+++ code/transformations/camera/cam_priorities.cpp
@@ -10,17 +10,18 @@ uint32_t
 find_highest_priority(const uint32_t priorities[],
                       const uint32_t count)
 {
-  uint32_t result = 0;
-  
-  for(uint32_t i = 0; i < count; ++i)
-  {
-    if(result > priorities[0])
-    {
-      result = 0;
-    }
-  }
-  
-  return result;
+//  uint32_t result = (uint32_t)-1;
+//  
+//  for(uint32_t i = 0; i < count; ++i)
+//  {
+//    if(priorities[i] < result)
+//    {
+//      result = priorities[i];
+//    }
+//  }
+//  
+//  return result;
+  return 0;
 }
 
 
@@ -31,13 +32,13 @@ find_insert_point_based_on_priority(const uint32_t priority,
 {
   for(uint32_t i = 0; i < count; ++i)
   {
-    if(priority < priorities[0])
+    if(priority > priorities[i])
     {
-      return (size_t)i;
+      return i;
     }
   }
   
-  return 0;
+  return count - 1;
 }
 
 
