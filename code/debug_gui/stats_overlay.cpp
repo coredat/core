e129e945dd830b469c549f208dfe5b98e00d498b
Phil Cooper-King <phil@cooperking.net>
2016-06-21 14:12:28 +0100

    Added some more stats, need tweeking tho.

--- code/debug_gui/stats_overlay.cpp
+++ code/debug_gui/stats_overlay.cpp
@@ -25,10 +25,27 @@ display_world_stats(const float dt,
       ImGui::End();
       return;
   }
+  
+  static float min_dt = dt;
+  static float max_dt = dt;
+  float curr_dt = dt;
+  
+  if(min_dt > curr_dt)
+  {
+    min_dt = curr_dt;
+  }
+  
+  if(max_dt < curr_dt)
+  {
+    max_dt = curr_dt;
+  }
 
   ImGui::Text("Stats");
   ImGui::Separator();
   ImGui::Text("Delta-time: %.4f", dt);
+  ImGui::Text("Min - Max dt: (%.2f, %.2f)", min_dt, max_dt);
+  ImGui::Text("Framerate: %d", static_cast<uint32_t>(1.f / dt));
+  ImGui::Text("Min - Max FPS: (%d, %d)", static_cast<uint32_t>(1.f / min_dt), static_cast<uint32_t>(1.f / max_dt));
   ImGui::Text("Delta-time multiplier: %.4f", dt_mul);
   ImGui::Text("Draw calls: %d", draw_calls);
   ImGui::Text("Render Passes: %d", render_passes);
