748004e14dd8a2c3cf8bd394d582456146244cf8
Phil Cooper-King <phil@cooperking.net>
2016-06-24 09:49:13 +0100

    Changed how input works, quick hack for current game, will likly change it later.

--- code/systems/sdl_backend/sdl_input.cpp
+++ code/systems/sdl_backend/sdl_input.cpp
@@ -103,10 +103,10 @@ update_keyboard_controller(Context_data::Game_controller *controller)
       float axis_01_x = 0;
       float axis_01_y = 0;
 
-      if(key_state[SDL_SCANCODE_A]) { axis_01_x -= 1; }
-      if(key_state[SDL_SCANCODE_D]) { axis_01_x += 1; }
-      if(key_state[SDL_SCANCODE_W]) { axis_01_y += 1; }
-      if(key_state[SDL_SCANCODE_S]) { axis_01_y -= 1; }
+      if(key_state[SDL_SCANCODE_A]) { axis_01_x = -1; }
+      if(key_state[SDL_SCANCODE_D]) { axis_01_x = +1; }
+      if(key_state[SDL_SCANCODE_W]) { axis_01_y = +1; }
+      if(key_state[SDL_SCANCODE_S]) { axis_01_y = -1; }
       
       controller->axis[0].x = axis_01_x;
       controller->axis[0].y = axis_01_y;
@@ -117,10 +117,10 @@ update_keyboard_controller(Context_data::Game_controller *controller)
       float axis_02_x = 0;
       float axis_02_y = 0;
 
-      if(key_state[SDL_SCANCODE_LEFT])  { axis_02_x -= 1; }
-      if(key_state[SDL_SCANCODE_RIGHT]) { axis_02_x += 1; }
-      if(key_state[SDL_SCANCODE_UP])    { axis_02_y += 1; }
-      if(key_state[SDL_SCANCODE_DOWN])  { axis_02_y -= 1; }
+      if(key_state[SDL_SCANCODE_LEFT])  { axis_02_x = -1; }
+      if(key_state[SDL_SCANCODE_RIGHT]) { axis_02_x = +1; }
+      if(key_state[SDL_SCANCODE_UP])    { axis_02_y = +1; }
+      if(key_state[SDL_SCANCODE_DOWN])  { axis_02_y = -1; }
       
       controller->axis[1].x = axis_02_x;
       controller->axis[1].y = axis_02_y;
