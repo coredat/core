5000fa8c68b6776f2708c78498122b5dc9462e9e
Phil Cooper-King <phil@cooperking.net>
2016-07-31 10:39:48 +0100

    Commenting out font stuff for the time.

--- code/core/font/font.cpp
+++ code/core/font/font.cpp
@@ -1,6 +1,8 @@
 #include <core/font/font.hpp>
 #include <data/global_data/resource_data.hpp>
 #include <data/global_data/font_data.hpp>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 namespace
@@ -14,45 +16,58 @@ namespace Core {
 
 Font::Font(const char *filename)
 {
-  if(!library)
-  {
-    auto error = FT_Init_FreeType( &library );
-    if ( error )
-    {
-      LOG_ERROR("Failed init freetype lib");
-    }
-  }
+//  long size;
+//  unsigned char* fontBuffer;
+//  
+//  FILE* fontFile = fopen(filename, "rb");
+//  fseek(fontFile, 0, SEEK_END);
+//  size = ftell(fontFile); /* how long is the file ? */
+//  fseek(fontFile, 0, SEEK_SET); /* reset */
+//  
+//  fontBuffer = (unsigned char*)malloc(size);
+//  
+//  fread(fontBuffer, size, 1, fontFile);
+//  fclose(fontFile);
 
-  FT_Face face;      /* handle to face object */
-  auto error = FT_New_Face(library,
-                            filename,
-                            0,
-                            &face);
-  if(error)
-  {
-    LOG_ERROR("Font err")
-  }
-  
-  auto resources = Resource_data::get_resources();
-  auto font = resources->font_data;
-  auto texture = resources->texture_data;
+//  if(!library)
+//  {
+//    auto error = FT_Init_FreeType( &library );
+//    if ( error )
+//    {
+//      LOG_ERROR("Failed init freetype lib");
+//    }
+//  }
 
-  Resource_data::data_lock(font);
-  Resource_data::data_lock(texture);
-  
-  Resource_data::font_data_push_back(font, font->size + 1);
-  Resource_data::font_data_set_property_font_face(font, font->size, face);
-  
-  auto texture_id = Resource_data::texture_data_push_back(texture);
-  
-  Ogl::Texture texture_glyphs;
-  Ogl::texture_create_2d(&texture_glyphs, 512, 512, GL_RED, true, nullptr);
-  
-  Resource_data::texture_data_set_property_texture(texture, texture_id, texture_glyphs);
-  Resource_data::font_data_set_property_texture_id(font, font->size, texture_id);
-  
-  Resource_data::data_unlock(texture);
-  Resource_data::data_unlock(font);
+//  FT_Face face;      /* handle to face object */
+//  auto error = FT_New_Face(library,
+//                            filename,
+//                            0,
+//                            &face);
+//  if(error)
+//  {
+//    LOG_ERROR("Font err")
+//  }
+//  
+//  auto resources = Resource_data::get_resources();
+//  auto font = resources->font_data;
+//  auto texture = resources->texture_data;
+//
+//  Resource_data::data_lock(font);
+//  Resource_data::data_lock(texture);
+//  
+//  Resource_data::font_data_push_back(font, font->size + 1);
+//  Resource_data::font_data_set_property_font_face(font, font->size, face);
+//  
+//  auto texture_id = Resource_data::texture_data_push_back(texture);
+//  
+//  Ogl::Texture texture_glyphs;
+//  Ogl::texture_create_2d(&texture_glyphs, 512, 512, GL_RED, true, nullptr);
+//  
+//  Resource_data::texture_data_set_property_texture(texture, texture_id, texture_glyphs);
+//  Resource_data::font_data_set_property_texture_id(font, font->size, texture_id);
+//  
+//  Resource_data::data_unlock(texture);
+//  Resource_data::data_unlock(font);
 }
 
 
--- code/core/renderer/text_renderer.cpp
+++ code/core/renderer/text_renderer.cpp
@@ -13,25 +13,25 @@ Text_renderer::Text_renderer()
 : m_font_id(util::generic_id_invalid())
 , m_text_id(util::generic_id_invalid())
 {
-  // Create a new text mesh
-  auto resources = Resource_data::get_resources();
-  assert(resources);
-  
-  auto text_mesh = resources->text_mesh_data;
-  assert(text_mesh);
-  
-  Resource_data::data_lock(text_mesh);
-  
-  LOG_TODO_ONCE("Dont create a mesh every time, check to see if it exists first");
-  
-  m_text_id = Resource_data::text_mesh_data_push_back(text_mesh);
-  
-  Graphics_api::Mesh mesh;
-  Resource_data::text_mesh_data_get_property_mesh(text_mesh, m_text_id, &mesh);
-  
-  Ogl::vertex_buffer_load(&mesh.vbo, nullptr, 0, 0, false);
-  
-  Resource_data::data_unlock(text_mesh);
+//  // Create a new text mesh
+//  auto resources = Resource_data::get_resources();
+//  assert(resources);
+//  
+//  auto text_mesh = resources->text_mesh_data;
+//  assert(text_mesh);
+//  
+//  Resource_data::data_lock(text_mesh);
+//  
+//  LOG_TODO_ONCE("Dont create a mesh every time, check to see if it exists first");
+//  
+//  m_text_id = Resource_data::text_mesh_data_push_back(text_mesh);
+//  
+//  Graphics_api::Mesh mesh;
+//  Resource_data::text_mesh_data_get_property_mesh(text_mesh, m_text_id, &mesh);
+//  
+//  Ogl::vertex_buffer_load(&mesh.vbo, nullptr, 0, 0, false);
+//  
+//  Resource_data::data_unlock(text_mesh);
 }
 
 
@@ -60,131 +60,132 @@ Text_renderer::get_font() const
 void
 Text_renderer::set_text(const char *str)
 {
-  // Generate the underlying resource
-  {
-    auto resources = Resource_data::get_resources();
-    assert(resources);
-    
-    auto text_mesh = resources->text_mesh_data;
-    assert(text_mesh);
-    
-    auto font = resources->font_data;
-    assert(font);
-    
-    auto texture = resources->texture_data;
-    assert(texture);
-    
-    auto glyphs = resources->glyphs_data;
-    assert(glyphs);
-    
-    // Get the font and build the data.
-    Resource_data::data_lock(font);
-    Resource_data::data_lock(text_mesh);
-    Resource_data::data_lock(texture);
-    Resource_data::data_lock(glyphs);
-    
-    LOG_TODO_ONCE("Dont create a mesh every time, check to see if it exists first");
-    
-    
-    Resource_data::text_mesh_data_set_property_text(text_mesh, m_text_id, str);
-
-    FT_Face face;
-    Resource_data::font_data_get_property_font_face(font, font->size, &face);
-    
-    if(face == nullptr)
-    {
-      return;
-    }
-    
-    util::generic_id texture_id;
-    Resource_data::font_data_get_property_texture_id(font, font->size, &texture_id);
-    
-    Ogl::Texture glyph_texture;
-    Resource_data::texture_data_get_property_texture(texture, texture_id, &glyph_texture);
-    
-
-    FT_Set_Pixel_Sizes(face, 0, 64);
-    
-    uint32_t char_index = 0;
-    char curr_char = str[char_index];
-    
-    // Actually need to check rasterised glyphs first.
-    
-    static int pointer_x = 0;
-    static int pointer_y = 0;
-    
-    while(curr_char != '\0')
-    {
-      // Check to see if the character exists already.
-      
-      
-      // If it doesn't exsit already add it
-      FT_Load_Char(face, curr_char, FT_LOAD_RENDER);
-      
-      if(face->glyph->bitmap.buffer)
-      {
-        const unsigned int width = face->glyph->bitmap.width;
-        const unsigned int height = face->glyph->bitmap.rows;
-      
-        Text::Character glyph_details;
-        glyph_details.uv[0] = math::to_float(pointer_x) / math::to_float(glyph_texture.width);
-        glyph_details.uv[1] = math::to_float(pointer_y) / math::to_float(glyph_texture.height);
-        
-        glyph_details.st[0] = math::to_float(pointer_x + width) / math::to_float(glyph_texture.width);
-        glyph_details.st[1] = math::to_float(pointer_y + height) / math::to_float(glyph_texture.height);
-        
-        glyph_details.advance[0] = face->glyph->advance.x;
-        glyph_details.advance[1] = face->glyph->advance.y;
-        
-//        assert(false); // this is where I'm at
-      
-        Ogl::texture_update_texture_2d(&glyph_texture,
-                                       pointer_x,
-                                       pointer_y,
-                                       width,
-                                       height,
-                                       face->glyph->bitmap.buffer);
-        
-        Resource_data::rasterized_glyphs_data_push_back(glyphs, (uint32_t)curr_char);
-        Resource_data::rasterized_glyphs_data_set_property_character(glyphs, (uint32_t)curr_char, glyph_details);
-        
-        pointer_x += width;
-      }
-      else
-      {
-        LOG_ERROR("Failed rasterizing glyph");
-      }
-      
-      curr_char = str[++char_index];
-    }
-    
-    
-    // Build mesh
-    {
-      LOG_TODO_ONCE("Font might not be set here!")
-      
-      uint32_t char_index = 0;
-      char curr_char = str[char_index];
-    
-      std::vector<float> data;
-      
-      float advance = 0;
-    
-      while(curr_char != '\0')
-      {
-        // Find glyph and build a vertex
-        Text::Character glyph_details;
-        Resource_data::rasterized_glyphs_data_get_property_character(glyphs, (util::generic_id)curr_char, &glyph_details);
-        
-        curr_char = str[++char_index];
-      }
-    }
-    
-    Resource_data::data_unlock(glyphs);
-    Resource_data::data_unlock(texture);
-    Resource_data::data_unlock(text_mesh);
-    Resource_data::data_unlock(font);
-  }
+//  return;
+//  // Generate the underlying resource
+//  {
+//    auto resources = Resource_data::get_resources();
+//    assert(resources);
+//    
+//    auto text_mesh = resources->text_mesh_data;
+//    assert(text_mesh);
+//    
+//    auto font = resources->font_data;
+//    assert(font);
+//    
+//    auto texture = resources->texture_data;
+//    assert(texture);
+//    
+//    auto glyphs = resources->glyphs_data;
+//    assert(glyphs);
+//    
+//    // Get the font and build the data.
+//    Resource_data::data_lock(font);
+//    Resource_data::data_lock(text_mesh);
+//    Resource_data::data_lock(texture);
+//    Resource_data::data_lock(glyphs);
+//    
+//    LOG_TODO_ONCE("Dont create a mesh every time, check to see if it exists first");
+//    
+//    
+//    Resource_data::text_mesh_data_set_property_text(text_mesh, m_text_id, str);
+//
+//    FT_Face face;
+//    Resource_data::font_data_get_property_font_face(font, font->size, &face);
+//    
+//    if(face == nullptr)
+//    {
+//      return;
+//    }
+//    
+//    util::generic_id texture_id;
+//    Resource_data::font_data_get_property_texture_id(font, font->size, &texture_id);
+//    
+//    Ogl::Texture glyph_texture;
+//    Resource_data::texture_data_get_property_texture(texture, texture_id, &glyph_texture);
+//    
+//
+//    FT_Set_Pixel_Sizes(face, 0, 64);
+//    
+//    uint32_t char_index = 0;
+//    char curr_char = str[char_index];
+//    
+//    // Actually need to check rasterised glyphs first.
+//    
+//    static int pointer_x = 0;
+//    static int pointer_y = 0;
+//    
+//    while(curr_char != '\0')
+//    {
+//      // Check to see if the character exists already.
+//      
+//      
+//      // If it doesn't exsit already add it
+//      FT_Load_Char(face, curr_char, FT_LOAD_RENDER);
+//      
+//      if(face->glyph->bitmap.buffer)
+//      {
+//        const unsigned int width = face->glyph->bitmap.width;
+//        const unsigned int height = face->glyph->bitmap.rows;
+//      
+//        Text::Character glyph_details;
+//        glyph_details.uv[0] = math::to_float(pointer_x) / math::to_float(glyph_texture.width);
+//        glyph_details.uv[1] = math::to_float(pointer_y) / math::to_float(glyph_texture.height);
+//        
+//        glyph_details.st[0] = math::to_float(pointer_x + width) / math::to_float(glyph_texture.width);
+//        glyph_details.st[1] = math::to_float(pointer_y + height) / math::to_float(glyph_texture.height);
+//        
+//        glyph_details.advance[0] = face->glyph->advance.x;
+//        glyph_details.advance[1] = face->glyph->advance.y;
+//        
+////        assert(false); // this is where I'm at
+//      
+//        Ogl::texture_update_texture_2d(&glyph_texture,
+//                                       pointer_x,
+//                                       pointer_y,
+//                                       width,
+//                                       height,
+//                                       face->glyph->bitmap.buffer);
+//        
+//        Resource_data::rasterized_glyphs_data_push_back(glyphs, (uint32_t)curr_char);
+//        Resource_data::rasterized_glyphs_data_set_property_character(glyphs, (uint32_t)curr_char, glyph_details);
+//        
+//        pointer_x += width;
+//      }
+//      else
+//      {
+//        LOG_ERROR("Failed rasterizing glyph");
+//      }
+//      
+//      curr_char = str[++char_index];
+//    }
+//    
+//    
+//    // Build mesh
+//    {
+//      LOG_TODO_ONCE("Font might not be set here!")
+//      
+//      uint32_t char_index = 0;
+//      char curr_char = str[char_index];
+//    
+//      std::vector<float> data;
+//      
+//      float advance = 0;
+//    
+//      while(curr_char != '\0')
+//      {
+//        // Find glyph and build a vertex
+//        Text::Character glyph_details;
+//        Resource_data::rasterized_glyphs_data_get_property_character(glyphs, (util::generic_id)curr_char, &glyph_details);
+//        
+//        curr_char = str[++char_index];
+//      }
+//    }
+//    
+//    Resource_data::data_unlock(glyphs);
+//    Resource_data::data_unlock(texture);
+//    Resource_data::data_unlock(text_mesh);
+//    Resource_data::data_unlock(font);
+//  }
 }
 
 
