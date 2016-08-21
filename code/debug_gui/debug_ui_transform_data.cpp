e2939b1dd6c964577b8c4aebb2b78e6fa6ff8bfa
Phil Cooper-King <phil@cooperking.net>
2016-08-21 12:58:13 +0100

    removed qu3e to add as sub-tree.

--- code/debug_gui/debug_ui_transform_data.cpp
+++ code/debug_gui/debug_ui_transform_data.cpp
@@ -37,11 +37,21 @@ display_transform_data(const World_data::Transform_data *data)
       // Get property_transform
       math::transform property_transform_value;
       World_data::transform_data_get_property_transform(data, data->transform_id[i], &property_transform_value);
-      ImGui::Text("Transform position: %f, %f, %f", math::get_x(property_transform_value.position), math::get_y(property_transform_value.position), math::get_z(property_transform_value.position));
-      ImGui::Text("Transform rotation: %f, %f, %f, %f", math::get_x(property_transform_value.rotation), math::get_y(property_transform_value.rotation), math::get_z(property_transform_value.rotation), math::get_w(property_transform_value.rotation));
-      ImGui::Text("Transform scale: %f, %f, %f", math::get_x(property_transform_value.scale), math::get_y(property_transform_value.scale), math::get_z(property_transform_value.scale));
+      ImGui::Text("Transform");
+      ImGui::Text("position: %.3f, %.3f, %.3f", math::get_x(property_transform_value.position), math::get_y(property_transform_value.position), math::get_z(property_transform_value.position));
+      ImGui::Text("rotation: %.3f, %.3f, %.3f, %.3f", math::get_x(property_transform_value.rotation), math::get_y(property_transform_value.rotation), math::get_z(property_transform_value.rotation), math::get_w(property_transform_value.rotation));
+      ImGui::Text("scale: %.3f, %.3f, %.3f", math::get_x(property_transform_value.scale), math::get_y(property_transform_value.scale), math::get_z(property_transform_value.scale));
 
 
+      // Get property_aabb
+      math::aabb property_aabb_value;
+      World_data::transform_data_get_property_aabb(data, data->transform_id[i], &property_aabb_value);
+      ImGui::Text("Aabb");
+      ImGui::Text("Min: %.3f, %.3f, %.3f", math::get_x(property_aabb_value.min), math::get_y(property_aabb_value.min), math::get_z(property_aabb_value.min));
+      ImGui::Text("Max: %.3f, %.3f, %.3f", math::get_x(property_aabb_value.max), math::get_y(property_aabb_value.max), math::get_z(property_aabb_value.max));
+
+      const math::vec3 origin = math::aabb_get_origin(property_aabb_value);
+      ImGui::Text("Origin: %.3f, %.3f, %.3f", math::get_x(origin), math::get_y(origin), math::get_z(origin));
 
 
       // End of data
