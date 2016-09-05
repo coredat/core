b6c83254b675657b4c779d6807c4d6f7112e00ae
Phil Cooper-King <phil@cooperking.net>
2016-09-05 10:02:47 +0100

    Adding break to utils, need to have a windows include.

--- code/external/utilities/utilities/assert.hpp
+++ code/external/utilities/utilities/assert.hpp
@@ -5,11 +5,41 @@
 #include <assert.h>
 
 
+/*
+  Assert on a condition.
+*/
 #define ASSERT_FAIL assert(false)
 #define ASSERT_FAIL_MSG(msg) assert(false)
 
 #define ASSERT(value) assert(value)
 #define ASSERT_MSG(value, msg) assert(value)
+
+
+/*
+  Stop exec is a in code break. Should hopefully allow you to resume
+  after the break, although this might be somewhat platform dependent.
+*/
+
+#ifdef __llvm__
+
+#if __has_builtin(__builtin_trap)
+#undef STOP_EXEC
+#define STOP_EXEC __builtin_trap();
+#endif
+
+#elif __GNUC__
+
+#if __has_feature(__builtin_trap)
+#undef STOP_EXEC
+#define STOP_EXEC __builtin_trap();
+#endif
+
+#else // Fallback to a warning.
+
+#include "logging.hpp"
+#define STOP_EXEC LOG_WARNING_ONCE("Stop_exec - Not supported on this platform.")
+
+#endif // STOP_EXEC
 
 
 #endif // inc guard
