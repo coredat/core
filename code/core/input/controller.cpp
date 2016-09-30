2fa1c9f8bbcee6a07688d89e4a164152d6b412d7
Phil Cooper-King <phil@cooperking.net>
2016-09-30 08:31:20 +0100

    Fixed controller_id check and added is_valid checks.

--- code/core/input/controller.cpp
+++ code/core/input/controller.cpp
@@ -28,7 +28,7 @@ Controller::Controller()
 Controller::Controller(const Core::Context &ctx, const uint32_t controller_id)
 : m_impl(nullptr)
 {
-  if(ctx.get_context_data()->input_pool->controller_count < controller_id)
+  if(ctx.get_context_data()->input_pool->controller_count > controller_id)
   {
     m_impl.reset(new Impl{controller_id, ctx.get_context_data()});
     assert(m_impl->context_data);
@@ -76,82 +76,85 @@ Controller::operator bool() const
 Axis
 Controller::get_axis(const uint8_t axis) const
 {
-  constexpr uint8_t max_axes = 2;
-  assert(axis < max_axes);
-
-  assert(m_impl && m_impl->context_data);
-  const Context_data::Input_pool *input = m_impl->context_data->input_pool;
-
-  if(input && input->controller_count > m_impl->controller_number && axis < max_axes)
+  if(is_valid())
   {
-    const Axis return_gp_axis = input->controllers[m_impl->controller_number].axis[axis];
-    
-    /*
-      If we are gamepad 0
-      Then check the mouse and keyboard if gp returned nothing to use.
-    */
-    if((m_impl->controller_number == 0) &&
-      return_gp_axis.x == 0.f &&
-      return_gp_axis.y == 0.f)
+    constexpr uint8_t max_axes = 2;
+    assert(axis < max_axes);
+
+    assert(m_impl && m_impl->context_data);
+    const Context_data::Input_pool *input = m_impl->context_data->input_pool;
+
+    if(input && input->controller_count > m_impl->controller_number && axis < max_axes)
     {
-      // We can think of mouse as axis 1
-      // ie head rotation.
-      if(axis == 1)
-      {
-        constexpr float scale_factor = 0.15f; // TODO: This should be customisable.
+      const Axis return_gp_axis = input->controllers[m_impl->controller_number].axis[axis];
       
-        const Axis mouse_axis = input->mice[0].delta;
-        const Axis normialized_axis = Axis{mouse_axis.x * scale_factor, mouse_axis.y * scale_factor};
-        
-        if(math::abs(normialized_axis.x) > 0.2f || math::abs(normialized_axis.y) > 0.2f)
+      /*
+        If we are gamepad 0
+        Then check the mouse and keyboard if gp returned nothing to use.
+      */
+      if((m_impl->controller_number == 0) &&
+        return_gp_axis.x == 0.f &&
+        return_gp_axis.y == 0.f)
+      {
+        // We can think of mouse as axis 1
+        // ie head rotation.
+        if(axis == 1)
         {
-          return normialized_axis;
+          constexpr float scale_factor = 0.15f; // TODO: This should be customisable.
+        
+          const Axis mouse_axis = input->mice[0].delta;
+          const Axis normialized_axis = Axis{mouse_axis.x * scale_factor, mouse_axis.y * scale_factor};
+          
+          if(math::abs(normialized_axis.x) > 0.2f || math::abs(normialized_axis.y) > 0.2f)
+          {
+            return normialized_axis;
+          }
+        }
+        
+        // We can think of wasd as movement.
+        // ie axis movement.
+        else if(axis == 0)
+        {
+          float x_axis = 0.f;
+          float y_axis = 0.f;
+          
+          using But_state = Core::Button_state;
+          
+          const But_state w_key = input->keyboard[SDL_SCANCODE_W];
+          const But_state a_key = input->keyboard[SDL_SCANCODE_A];
+          const But_state s_key = input->keyboard[SDL_SCANCODE_S];
+          const But_state d_key = input->keyboard[SDL_SCANCODE_D];
+          
+          if(w_key == But_state::down || w_key == Button_state::down_on_frame)
+          {
+            y_axis += 1.f;
+          }
+          if(a_key == But_state::down || a_key == Button_state::down_on_frame)
+          {
+            x_axis -= 1.f;
+          }
+          if(s_key == But_state::down || s_key == Button_state::down_on_frame)
+          {
+            y_axis -= 1.f;
+          }
+          if(d_key == But_state::down || d_key == Button_state::down_on_frame)
+          {
+            x_axis += 1.f;
+          }
+          
+          // Normalize them so similar to analog stick.
+          if(math::abs(x_axis) == 1.f && math::abs(y_axis) == 1.f)
+          {
+            x_axis = math::sign(x_axis) * math::root_two();
+            y_axis = math::sign(y_axis) * math::root_two();
+          }
+          
+          return Axis{x_axis, y_axis};
         }
       }
       
-      // We can think of wasd as movement.
-      // ie axis movement.
-      else if(axis == 0)
-      {
-        float x_axis = 0.f;
-        float y_axis = 0.f;
-        
-        using But_state = Core::Button_state;
-        
-        const But_state w_key = input->keyboard[SDL_SCANCODE_W];
-        const But_state a_key = input->keyboard[SDL_SCANCODE_A];
-        const But_state s_key = input->keyboard[SDL_SCANCODE_S];
-        const But_state d_key = input->keyboard[SDL_SCANCODE_D];
-        
-        if(w_key == But_state::down || w_key == Button_state::down_on_frame)
-        {
-          y_axis += 1.f;
-        }
-        if(a_key == But_state::down || a_key == Button_state::down_on_frame)
-        {
-          x_axis -= 1.f;
-        }
-        if(s_key == But_state::down || s_key == Button_state::down_on_frame)
-        {
-          y_axis -= 1.f;
-        }
-        if(d_key == But_state::down || d_key == Button_state::down_on_frame)
-        {
-          x_axis += 1.f;
-        }
-        
-        // Normalize them so similar to analog stick.
-        if(math::abs(x_axis) == 1.f && math::abs(y_axis) == 1.f)
-        {
-          x_axis = math::sign(x_axis) * math::root_two();
-          y_axis = math::sign(y_axis) * math::root_two();
-        }
-        
-        return Axis{x_axis, y_axis};
-      }
+      return return_gp_axis;
     }
-    
-    return return_gp_axis;
   }
 
   return Axis();
@@ -161,32 +164,37 @@ Controller::get_axis(const uint8_t axis) const
 float
 Controller::get_trigger(const uint8_t trigger) const
 {
-  assert(m_impl && m_impl->context_data);
-  
-  const Context_data::Input_pool *input = m_impl->context_data->input_pool;
-  assert(input);
-  
-  constexpr uint8_t max_triggers = 2;
-  
-  float return_value = 0.f;
-
-  if(input && input->controller_count > m_impl->controller_number && trigger < max_triggers)
+  if(is_valid())
   {
-    return_value = input->controllers[m_impl->controller_number].triggers[trigger];
+    assert(m_impl && m_impl->context_data);
     
-    // If primary gamepad also check left mouse button.
-    if(m_impl->controller_number == 0 && return_value == 0.f)
+    const Context_data::Input_pool *input = m_impl->context_data->input_pool;
+    assert(input);
+    
+    constexpr uint8_t max_triggers = 2;
+    
+    float return_value = 0.f;
+
+    if(input && input->controller_count > m_impl->controller_number && trigger < max_triggers)
     {
-      const Core::Button_state *buttons = &input->mice[0].buttons[0];
-    
-      if(buttons[0] == Button_state::down_on_frame || buttons[0] == Button_state::down)
+      return_value = input->controllers[m_impl->controller_number].triggers[trigger];
+      
+      // If primary gamepad also check left mouse button.
+      if(m_impl->controller_number == 0 && return_value == 0.f)
       {
-        return_value = 1.f;
+        const Core::Button_state *buttons = &input->mice[0].buttons[0];
+      
+        if(buttons[0] == Button_state::down_on_frame || buttons[0] == Button_state::down)
+        {
+          return_value = 1.f;
+        }
       }
     }
-  }
 
-  return return_value;
+    return return_value;
+  }
+  
+  return 0.f;
 }
 
 
@@ -355,44 +363,64 @@ is_button(Context_data::Input_pool *input,
 bool
 Controller::is_button_down(const uint32_t buttons) const
 {
-  return is_button(m_impl->context_data->input_pool,
-                   buttons,
-                   (uint32_t)Core::Button_state::down,
-                   (uint32_t)Core::Button_state::down_on_frame,
-                   m_impl->controller_number);
+  if(is_valid())
+  {
+    return is_button(m_impl->context_data->input_pool,
+                     buttons,
+                     (uint32_t)Core::Button_state::down,
+                     (uint32_t)Core::Button_state::down_on_frame,
+                     m_impl->controller_number);
+  }
+  
+  return false;
 }
 
 
 bool
 Controller::is_button_down_on_frame(const uint32_t buttons) const
 {
-  return is_button(m_impl->context_data->input_pool,
-                   buttons,
-                   (uint32_t)Core::Button_state::down_on_frame,
-                   (uint32_t)Core::Button_state::down_on_frame,
-                   m_impl->controller_number);
+  if(is_valid())
+  {
+    return is_button(m_impl->context_data->input_pool,
+                     buttons,
+                     (uint32_t)Core::Button_state::down_on_frame,
+                     (uint32_t)Core::Button_state::down_on_frame,
+                     m_impl->controller_number);
+  }
+  
+  return false;
 }
  
 
 bool
 Controller::is_button_up(const uint32_t buttons) const
 {
-  return is_button(m_impl->context_data->input_pool,
-                   buttons,
-                   (uint32_t)Core::Button_state::up,
-                   (uint32_t)Core::Button_state::up_on_frame,
-                   m_impl->controller_number);
+  if(is_valid())
+  {
+    return is_button(m_impl->context_data->input_pool,
+                     buttons,
+                     (uint32_t)Core::Button_state::up,
+                     (uint32_t)Core::Button_state::up_on_frame,
+                     m_impl->controller_number);
+  }
+  
+  return false;
 }
 
 
 bool
 Controller::is_button_up_on_frame(const uint32_t buttons) const
 {
-  return is_button(m_impl->context_data->input_pool,
-                   buttons,
-                   (uint32_t)Core::Button_state::up_on_frame,
-                   (uint32_t)Core::Button_state::up_on_frame,
-                   m_impl->controller_number);
+  if(is_valid())
+  {
+    return is_button(m_impl->context_data->input_pool,
+                     buttons,
+                     (uint32_t)Core::Button_state::up_on_frame,
+                     (uint32_t)Core::Button_state::up_on_frame,
+                     m_impl->controller_number);
+  }
+  
+  return false;  
 }
 
 
