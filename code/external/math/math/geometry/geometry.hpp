710b95a19ad55ce4b015d781f8e49cb270967dd4
Phil Cooper-King <phil@cooperking.net>
2017-03-17 07:23:46 +0000

    Math aabb init function.

--- code/external/math/math/geometry/geometry.hpp
+++ code/external/math/math/geometry/geometry.hpp
@@ -2,8 +2,32 @@
 #define GEOMETRY_INCLUDED_98853631_728E_4E3F_AC27_065504B13942
 
 
+#include "../detail/detail.hpp"
 #include "geometry_types.hpp"
+#include "../vec/vec3.hpp"
 #include "aabb.hpp"
+
+
+_MATH_NS_OPEN
+
+
+// ----------------------------------------------------------- [ Interface ] --
+
+
+MATH_GENR_INLINE aabb            aabb_init();
+
+
+// ---------------------------------------------------------------- [ Impl ] --
+
+
+aabb
+aabb_init()
+{
+  return { math::vec3_zero(), math::vec3_zero() };
+}
+
+
+_MATH_NS_CLOSE
 
 
 #endif // inc guard
